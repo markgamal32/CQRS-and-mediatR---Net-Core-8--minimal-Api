e4ce4189675b0c32f040f04ff32bca9e3167d92e
markgamal32 <96923190+markgamal32@users.noreply.github.com>
2024-09-05 20:41:51 +0300

    Add Product DTO

---
+++ Features/Products/DTOs/ProductDto.cs
@@ -0,0 +1,11 @@
+﻿namespace CQRSMediatR.Features.Products.DTOs
+{
+	public record ProductDto(Guid Id, string Name, string Description, decimal Price);
+	/* Quick Tip: It’s recommended to use records to define Data Transfer Objects,
+	   as they are Immutable by default! 
+	   Used only to transfer states , and nothing else 
+	   One-Way Flow 
+	*/
+
+
+}
