d6e9305d845302c0a6933c4d2a35d0641fdd7680
markgamal32 <96923190+markgamal32@users.noreply.github.com>
2024-09-05 20:49:45 +0300

    Queries List All Products Get Product By ID

---
+++ Features/Products/Queries/Get/GetProductQuery.cs
@@ -0,0 +1,7 @@
+﻿using CQRSMediatR.Features.Products.DTOs;
+using MediatR;
+
+namespace CQRSMediatR.Features.Products.Queries.Get
+{
+	public record GetProductQuery(Guid Id) : IRequest<ProductDto>;
+}
