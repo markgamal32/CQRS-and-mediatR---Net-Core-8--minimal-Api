23a4b44af36f026be79505215c0e1c08380c2346
markgamal32 <96923190+markgamal32@users.noreply.github.com>
2024-09-06 03:29:25 +0300

    -Delete Product By ID -DeleteProductCommand -DeleteProductCommandHandler

---
+++ Features/Products/Commands/Delete/DeleteProductCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace CQRSMediatR.Features.Products.Commands.Delete
+{
+	public record DeleteProductCommand(Guid Id) : IRequest;
+
+	/*The Delete Handler would take in the ID, 
+	 * fetch the record from the database, and try to remove it.
+	 * If the product with the mentioned ID is not found, it simply exits out of the handler code.
+    */
+
+}
