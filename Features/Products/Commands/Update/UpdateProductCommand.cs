77a945ce8baecddf84eb160e1c8a53d99c80cf16
markgamal32 <96923190+markgamal32@users.noreply.github.com>
2024-09-06 20:29:48 +0300

    Update Product -UpdateProductCommand -UpdateProductCommandHandler

---
+++ Features/Products/Commands/Update/UpdateProductCommand.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+
+namespace CQRSMediatR.Features.Products.Commands.Update
+{
+	public record UpdateProductCommand(Guid  Id,string Name, string Description, decimal Price) : IRequest;
+
+}
---
+++ Features/Products/Commands/Update/UpdateProductCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using CQRSMediatR.Features.Products.Commands.Create;
+using CQRSMediatR.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace CQRSMediatR.Features.Products.Commands.Update
+{
+	public class UpdateProductCommandHandler(AppDbContext context) : IRequestHandler<UpdateProductCommand>
+	{
+		public async Task Handle(UpdateProductCommand request, CancellationToken cancellationToken)
+		{
+			var product = await context.Products.FindAsync(new object[] { request.Id }, cancellationToken);
+			if (product == null)
+			{
+				// Throwing an exception if the product does not exist
+				throw new KeyNotFoundException($"Product with ID {request.Id} was not found.");
+			}
+
+			// Update the product properties
+			product.Name = request.Name;
+			product.Description = request.Description;
+			product.Price = request.Price;
+
+			await context.SaveChangesAsync(cancellationToken);
+		
+
+	}
+	}
+}
